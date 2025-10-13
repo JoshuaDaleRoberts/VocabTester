23e8f3da16f9c5647d93baa4538b23ca3dfa33ba
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2025-10-13 13:59:57 -0400

    add pickle support

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import webbrowser
 from pathlib import Path
 
+import pickle
+
 file_path = Path("index.html").resolve()
 webbrowser.open(file_path.as_uri(), new=2)
 
 items = ["1st Period", "2nd Period", "3rd Period", "4th Period", "5th Period", "6th Period", "7th Period"]
 
+with open("items.pkl", "wb") as f:
+    pickle.dump(items, f)
+
+
+b = list()
+
+with open("items.pkl", "rb") as f:
+    b = pickle.load(f)
+
 html_path = Path("index.html")
 temp_path = Path("temp.html")
 html = html_path.read_text(encoding="utf-8")
 
-li_items = "\n".join(f"    <li>\n        {item}\n    </li>" for item in items)
+li_items = "\n".join(f"        <li>\n            {item}\n        </li>" for item in b)
 
 html = html.replace("<!-- ITEMS -->", li_items)
 temp_path.write_text(html, encoding="utf-8")
