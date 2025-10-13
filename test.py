c5b9754660a65515556887de54fdfb10e750e9f0
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2025-10-13 14:51:47 -0400

    button that console logs on server

--- test.py
+++ test.py
@@ -4,7 +4,41 @@ import socketserver
 port = 2704
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", port), Handler) as httpd:
-    print("serving at port", port)
-    httpd.serve_forever()
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"""
+        <html>
+        <body>
+            <form method="POST">
+                <button type="submit">Hello World</button>
+            </form>
+        </body>
+        </html>
+        """)
+
+    def do_POST(self):
+        print("hello world")  # This prints to the console
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"""
+        <html>
+        <body>
+            <form method="POST">
+                <button type="submit">Hello World</button>
+            </form>
+        </body>
+        </html>
+        """)
+if __name__ == "__main__":
+    server = HTTPServer(('localhost', 8000), SimpleHandler)
+    print("Server running on http://localhost:8000")
+    server.serve_forever()
+
+
 
