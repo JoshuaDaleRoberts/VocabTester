2c2589118b29cfed42fb83a8db88d4c92f9f08f1
JoshuaDaleRoberts <joshdroberts04@gmail.com>
2026-03-04 18:12:45 -0500

    sanitize upload

--- test.py
+++ test.py
@@ -417,6 +417,9 @@ class SimpleHandler(BaseHTTPRequestHandler):
             text = text.lower()
             text = ''.join([i for i in text if i.isalpha() or i.isspace()])
 
+            #GREAT MERGER sanitize and remove existing words from text
+            test = classList[class_number].sanitize_text(text)
+
             first_word = text.split(" ")[0]
             rest_of_text = " ".join(text.split()[1:])
 
